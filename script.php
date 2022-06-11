8c10a01f1bd11dc3bfb852e17dbc7e46dc3a0a99
Enrico <enricoferreiradossantos@gmail.com>
2022-06-11 20:12:46 -0300

    validacao simples

--- script.php
+++ script.php
@@ -3,34 +3,45 @@
 echo "<body style='background: black; color: white;'></body>" ;
 
 $categorias = [];
-$categorias[] = 'Infantil';
-$categorias[] = 'Adolescente';
-$categorias[] = 'Adulto';
-$categorias[] = 'Idoso';
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
 //print_r($categorias);
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-//var_dump($nome);
-//var_dump($idade);
+if (empty($nome)) {
+	echo "O nome não pode estar vazio";
+	return;
+}
+
+if (strlen($nome) < 3) {
+	echo "O nome deve conter mais de 3 caracteres";
+	return;
+}
+
+if (strlen($nome) > 40) {
+	echo "O nome é muito extenso";
+	return;
+}
 
 if ($idade >= 6 && $idade <= 12) {
 	for ($i=0; $i < count($categorias); $i++) { 
-		if ($categorias[$i] == 'Infantil')  
+		if ($categorias[$i] == 'infantil')  
 			echo "O nadador " . $nome . " compete na caragoria " .$categorias[$i];
 	}
 
 }
 elseif ($idade >= 13 && $idade <= 18) {
 	for ($i=0; $i < count($categorias); $i++) { 
-		if ($categorias[$i] == 'Adolescente')  
-			echo "O nadador " . $nome . " compete na caragoria Adolescente";
+		if ($categorias[$i] == 'adolescente')  
+			echo "O nadador " . $nome . " compete na caragoria adolescente";
 	}
 }
 else {
 	for ($i=0; $i < count($categorias); $i++) { 
-		if ($categorias[$i] == 'Adulto')  
-			echo "O nadador " . $nome . " compete na caragoria Adulto";
+		if ($categorias[$i] == 'adulto')  
+			echo "O nadador " . $nome . " compete na caragoria adulto";
 	}
 }
