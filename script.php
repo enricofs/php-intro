87b182f37f5f03298ceccf8a29302f20a450f74b
Enrico <enricoferreiradossantos@gmail.com>
2022-06-11 20:23:44 -0300

    Finalizando validação de campos do formulário

--- script.php
+++ script.php
@@ -26,6 +26,11 @@ if (strlen($nome) > 40) {
 	return;
 }
 
+if (!is_numeric($idade)) {
+	echo "Informe um número para idade";
+	return;
+}
+
 if ($idade >= 6 && $idade <= 12) {
 	for ($i=0; $i < count($categorias); $i++) { 
 		if ($categorias[$i] == 'infantil')  
